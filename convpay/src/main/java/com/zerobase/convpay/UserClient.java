235fea1a73f7e7076f9397d7ca0778c904ea28c5
rlagkdud <ha0936861@gmail.com>
2023-05-03 15:17:51 +0900

    change to Spring project

--- convpay/src/main/java/com/zerobase/convpay/UserClient.java
+++ convpay/src/main/java/com/zerobase/convpay/UserClient.java
@@ -8,12 +8,16 @@ import com.zerobase.convpay.dto.PayResponse;
 import com.zerobase.convpay.service.ConveniencePayService;
 import com.zerobase.convpay.type.ConvenienceType;
 import com.zerobase.convpay.type.PayMethodType;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class UserClient {
     public static void main(String[] args) {
         // '사용자' -> 결제시스템 -> 머니 어댑터
-        ApplicationConfig applicationConfig = new ApplicationConfig();
-        ConveniencePayService conveniencePayService = applicationConfig.conveniencePayServiceDiscountByPayMethod();
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        ConveniencePayService conveniencePayService =
+                applicationContext.getBean("conveniencePayService", ConveniencePayService.class);
 
         // G25 편의점에서 결제 1000원
         PayRequest payRequest = new PayRequest(PayMethodType.CARD, ConvenienceType.G25, 50);
--- convpay/src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
+++ convpay/src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
@@ -1,26 +1,36 @@
 package com.zerobase.convpay.config;
 
 import com.zerobase.convpay.service.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
+@Configuration
 public class ApplicationConfig {
 
-    public ConveniencePayService conveniencePayServiceDiscountByConvenience(){
+    @Bean
+    public ConveniencePayService conveniencePayService(){
         return new ConveniencePayService(
                 new HashSet<>(
-                        Arrays.asList(new MoneyAdapter(), new CardAdapter())
+                        Arrays.asList(moneyAdapter(), cardAdapter())
                 ),
-                new DiscountByConvenience()
+                discountByConvenience()
         );
     }
-    public ConveniencePayService conveniencePayServiceDiscountByPayMethod(){
-        return new ConveniencePayService(
-                new HashSet<>(
-                        Arrays.asList(new MoneyAdapter(), new CardAdapter())
-                ),
-                new DiscountByPayMethod()
-        );
+    @Bean
+    public static CardAdapter cardAdapter() {
+        return new CardAdapter();
+    }
+
+    @Bean
+    public static MoneyAdapter moneyAdapter() {
+        return new MoneyAdapter();
+    }
+
+    @Bean
+    public static DiscountByConvenience discountByConvenience() {
+        return new DiscountByConvenience();
     }
 }
