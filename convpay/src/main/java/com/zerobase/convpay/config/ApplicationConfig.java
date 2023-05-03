4babb4ae7a96d83477e21805f410aff0dc03a9a4
rlagkdud <ha0936861@gmail.com>
2023-05-03 17:25:26 +0900

    JavaConfig ComponentScan 방식으로 Bean 등록

--- convpay/src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
+++ convpay/src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
@@ -1,36 +1,17 @@
 package com.zerobase.convpay.config;
 
+import com.zerobase.convpay.ConvpayApplication;
 import com.zerobase.convpay.service.*;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
 @Configuration
+@ComponentScan(basePackageClasses= ConvpayApplication.class )
 public class ApplicationConfig {
 
-    @Bean
-    public ConveniencePayService conveniencePayService(){
-        return new ConveniencePayService(
-                new HashSet<>(
-                        Arrays.asList(moneyAdapter(), cardAdapter())
-                ),
-                discountByConvenience()
-        );
-    }
-    @Bean
-    public static CardAdapter cardAdapter() {
-        return new CardAdapter();
-    }
 
-    @Bean
-    public static MoneyAdapter moneyAdapter() {
-        return new MoneyAdapter();
-    }
-
-    @Bean
-    public static DiscountByConvenience discountByConvenience() {
-        return new DiscountByConvenience();
-    }
 }
