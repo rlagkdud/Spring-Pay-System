8a9cbfe7c2ea9458853cee3b81db5196e4448226
rlagkdud <ha0936861@gmail.com>
2023-05-03 18:18:59 +0900

    autowired 사용 예시

--- convpay/src/main/java/com/zerobase/convpay/service/DiscountByConvenience.java
+++ convpay/src/main/java/com/zerobase/convpay/service/DiscountByConvenience.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("test")
+@Primary
 public class DiscountByConvenience implements DiscountInterface{
     @Override
     public Integer getDiscountAmount(PayRequest payRequest) {
--- convpay/src/main/java/com/zerobase/convpay/service/DiscountByPayMethod.java
+++ convpay/src/main/java/com/zerobase/convpay/service/DiscountByPayMethod.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("production")
 public class DiscountByPayMethod implements DiscountInterface {
     @Override
     public Integer getDiscountAmount(PayRequest payRequest) {
