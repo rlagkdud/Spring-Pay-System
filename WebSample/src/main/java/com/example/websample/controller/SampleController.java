6f5117b1127c1df9468ae2e52bd6cb8662280b8e
rlagkdud <ha0936861@gmail.com>
2023-05-08 12:03:08 +0900

    http 요청 매핑 - get, post : using 축약형 매핑 어노테이션

--- WebSample/src/main/java/com/example/websample/controller/SampleController.java
+++ WebSample/src/main/java/com/example/websample/controller/SampleController.java
@@ -1,17 +1,20 @@
 package com.example.websample.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 public class SampleController {
 
-    @RequestMapping(value = "/order/1", method = RequestMethod.GET)
+    @GetMapping("/order/1")
     public String getOrder(){
         log.info("Get some order");
         return "orderId:1, orderAmount:1000";
+    }
+    @PostMapping("/order")
+    public String createOrder(){
+        log.info("Create order");
+        return "order created -> orderId:1, orderAmount:1000";
     }
 }
