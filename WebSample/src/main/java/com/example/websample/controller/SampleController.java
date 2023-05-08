8c8de7cfce7575315e17f2a281472b200cd695ce
rlagkdud <ha0936861@gmail.com>
2023-05-08 13:37:55 +0900

    http 요청 파라미터 전송 - parameterVariable, requestParam (GET, DELETE)

--- WebSample/src/main/java/com/example/websample/controller/SampleController.java
+++ WebSample/src/main/java/com/example/websample/controller/SampleController.java
@@ -7,13 +7,28 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class SampleController {
 
-    @GetMapping("/order/1")
-    public String getOrder(){
-        log.info("Get some order");
-        return "orderId:1, orderAmount:1000";
+    @GetMapping("/order/{orderId}")
+    public String getOrder(@PathVariable("orderId") String id) {
+        log.info("Get some order information: " + id);
+        return "orderId:" + id + ", orderAmount:1000";
     }
+
+    @DeleteMapping ("/order/{orderId}")
+    public String deleteOrder(@PathVariable("orderId") String id) {
+        log.info("delete order: " + id);
+        return "delete orderId:" + id;
+    }
+
+    @GetMapping("/order")
+    public String getOrderWitRequestParam(
+            @RequestParam(value = "orderId", required = false, defaultValue = "defaultId") String id,
+            @RequestParam("orderAmount") Integer amount) {
+        log.info("Get order id: " + id+", amount: "+amount);
+        return "orderId:" + id + ", orderAmount:"+amount;
+    }
+
     @PostMapping("/order")
-    public String createOrder(){
+    public String createOrder() {
         log.info("Create order");
         return "order created -> orderId:1, orderAmount:1000";
     }
