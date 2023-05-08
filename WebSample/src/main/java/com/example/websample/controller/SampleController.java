7019cf64a2410e32045504d4e3711cdd6832bf1f
rlagkdud <ha0936861@gmail.com>
2023-05-08 17:32:53 +0900

    예외처리 - global

--- WebSample/src/main/java/com/example/websample/controller/SampleController.java
+++ WebSample/src/main/java/com/example/websample/controller/SampleController.java
@@ -33,33 +33,6 @@ public class SampleController {
         return "orderId:" + id;
     }
 
-    @ExceptionHandler(IllegalAccessException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalAccessException(IllegalAccessException e){
-        log.error("IllegalAccessException is occurred", e);
-
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                .body(new ErrorResponse(ErrorCode.TOO_BIG_ID_ERROR,
-                        "IllegalAccessException is occurred"));
-    }
-
-    @ExceptionHandler(WebSampleException.class)
-    public ResponseEntity<ErrorResponse> handleWebSampleException(WebSampleException e){
-        log.error("WebSampleException is occurred", e);
-
-        return ResponseEntity.status(HttpStatus.INSUFFICIENT_STORAGE)
-                .body(new ErrorResponse(e.getErrorCode(),
-                        "WebSampleException is occurred"));
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e){
-        log.error("Exception is occurred", e);
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR,
-                        "Exception is occurred"));
-    }
-
     @DeleteMapping("/order/{orderId}")
     public String deleteOrder(@PathVariable("orderId") String id) {
         log.info("delete order: " + id);
--- WebSample/src/main/java/com/example/websample/exception/ErrorCode.java
+++ WebSample/src/main/java/com/example/websample/exception/ErrorCode.java
@@ -2,6 +2,8 @@ package com.example.websample.exception;
 
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR,
+    NEW_OLD_UNMATCH,
+    REFUND_AMOUNT_EXCEED_PAYMENT_AMOUNT,
     TOO_BIG_ID_ERROR,
     TOO_SMALL_ID_ERROR
 }
