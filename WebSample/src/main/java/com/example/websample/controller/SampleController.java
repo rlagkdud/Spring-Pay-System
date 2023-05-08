81c0dcc1cd3fb116c47577b60b96efd35567d82e
rlagkdud <ha0936861@gmail.com>
2023-05-08 13:56:48 +0900

    http 요청 파라미터 전송 - RequestBody, RequestHeader (POST, PUT, PATCH)

--- WebSample/src/main/java/com/example/websample/controller/SampleController.java
+++ WebSample/src/main/java/com/example/websample/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.example.websample.controller;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +14,7 @@ public class SampleController {
         return "orderId:" + id + ", orderAmount:1000";
     }
 
-    @DeleteMapping ("/order/{orderId}")
+    @DeleteMapping("/order/{orderId}")
     public String deleteOrder(@PathVariable("orderId") String id) {
         log.info("delete order: " + id);
         return "delete orderId:" + id;
@@ -23,13 +24,30 @@ public class SampleController {
     public String getOrderWitRequestParam(
             @RequestParam(value = "orderId", required = false, defaultValue = "defaultId") String id,
             @RequestParam("orderAmount") Integer amount) {
-        log.info("Get order id: " + id+", amount: "+amount);
-        return "orderId:" + id + ", orderAmount:"+amount;
+        log.info("Get order id: " + id + ", amount: " + amount);
+        return "orderId:" + id + ", orderAmount:" + amount;
     }
 
     @PostMapping("/order")
+    public String createOrder(
+            @RequestBody CreateOrderRequest createOrderRequest,
+            @RequestHeader String userAccountId) {
+        log.info("createOrderReq: " + createOrderRequest +
+                ", userAccountId: " + userAccountId);
+        return "orderId:" + createOrderRequest.getOrderId() +
+                ", orderAmount:" + createOrderRequest.getOrderAmount();
+    }
+
+    @PutMapping("/order")
     public String createOrder() {
         log.info("Create order");
         return "order created -> orderId:1, orderAmount:1000";
+    }
+
+    @Data
+    public static class CreateOrderRequest {
+        private String orderId;
+        private Integer orderAmount;
+
     }
 }
