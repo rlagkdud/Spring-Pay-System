e0123abb667fae9f042020b1edf24945981b8f16
rlagkdud <ha0936861@gmail.com>
2023-05-09 21:25:17 +0900

    Controller test code 작성 - @WebMvcTest

---
+++ Account/src/test/java/com/example/account/controller/AccountControllerTest.java
@@ -0,0 +1,46 @@
+package com.example.account.controller;
+
+import com.example.account.domain.Account;
+import com.example.account.domain.AccountStatus;
+import com.example.account.service.AccountService;
+import com.example.account.service.RedisService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AccountController.class)
+class AccountControllerTest {
+    @MockBean
+    private AccountService accountService;
+    @MockBean
+    private RedisService redisService;
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    void successGetAccount () throws Exception {
+        // given
+        given(accountService.getAccount(anyLong()))
+                .willReturn(Account.builder()
+                        .accountNumber("3456")
+                        .accountStatus(AccountStatus.IN_USE)
+                        .build());
+        // when
+        // then
+        mockMvc.perform(get("/account/123"))
+                .andDo(print())
+                .andExpect(jsonPath("$.accountNumber").value("3456"))
+                .andExpect(jsonPath("$.accountStatus").value("IN_USE"))
+                .andExpect(status().isOk());
+    }
+}
