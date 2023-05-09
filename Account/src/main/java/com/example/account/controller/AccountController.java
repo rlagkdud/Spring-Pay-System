4817ae2fe9eaceb2237fec332db62dca1918c0f5
rlagkdud <ha0936861@gmail.com>
2023-05-09 15:29:32 +0900

    h2 db에 값 저장하기

---
+++ Account/src/main/java/com/example/account/controller/AccountController.java
@@ -0,0 +1,18 @@
+package com.example.account.controller;
+
+import com.example.account.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AccountController {
+    private final AccountService accountService;
+
+    @GetMapping("/create-account")
+    public String createAccount(){
+        accountService.createAccount();
+        return "success";
+    }
+}
---
+++ Account/src/main/java/com/example/account/service/AccountService.java
@@ -0,0 +1,23 @@
+package com.example.account.service;
+
+import com.example.account.domain.Account;
+import com.example.account.domain.AccountStatus;
+import com.example.account.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    @Transactional
+    public void createAccount(){
+        Account account = Account.builder()
+                .accountNumber("40000")
+                .accountStatus(AccountStatus.IN_USE)
+                .build();
+        accountRepository.save(account);
+    }
+}
