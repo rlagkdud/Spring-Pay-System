88bb8dd5295d5c16a9c06c085576575ea59c453b
rlagkdud <ha0936861@gmail.com>
2023-05-09 18:02:38 +0900

    test code 작성 - @BeforeEach, @DisplayName

--- Account/src/test/java/com/example/account/service/AccountServiceTest.java
+++ Account/src/test/java/com/example/account/service/AccountServiceTest.java
@@ -2,6 +2,8 @@ package com.example.account.service;
 
 import com.example.account.domain.Account;
 import com.example.account.domain.AccountStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,11 +15,21 @@ class AccountServiceTest {
     @Autowired
     private AccountService accountService;
 
-
-    @Test
-    void testGetAccount(){
+    @BeforeEach
+    void init(){
         accountService.createAccount();
+    }
+    @Test
+    @DisplayName("테스트 이름 변경")
+    void testGetAccount(){
         Account account = accountService.getAccount(1L);
+
+        assertEquals("40000", account.getAccountNumber());
+        assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
+    }
+    @Test
+    void testGetAccount2(){
+        Account account = accountService.getAccount(2L);
 
         assertEquals("40000", account.getAccountNumber());
         assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
