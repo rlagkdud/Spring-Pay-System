cc584feaf3d214a445d5594c2b848e0f4c42e65d
rlagkdud <ha0936861@gmail.com>
2023-05-11 16:45:46 +0900

    계좌 생성 - Test service.createAccount() with Test code

--- Account/src/test/java/com/example/account/service/AccountServiceTest.java
+++ Account/src/test/java/com/example/account/service/AccountServiceTest.java
@@ -1,6 +1,9 @@
 package com.example.account.service;
 
 import com.example.account.domain.Account;
+import com.example.account.domain.AccountUser;
+import com.example.account.dto.AccountDto;
+import com.example.account.repository.AccountUserRepository;
 import com.example.account.type.AccountStatus;
 import com.example.account.repository.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -24,74 +27,38 @@ import static org.mockito.Mockito.verify;
 class AccountServiceTest {
     @Mock
     private AccountRepository accountRepository;
+    @Mock
+    private AccountUserRepository accountUserRepository;
     @InjectMocks
     private AccountService accountService;
 
     @Test
-    @DisplayName("계좌 조회 성공")
-    void testXXX(){
+    void createAccountSuccess () {
         // given
-        given(accountRepository.findById(anyLong()))
+        AccountUser user = AccountUser.builder()
+                .id(12L)
+                .name("Pobi").build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+
+        given(accountRepository.findFirstByOrderByIdDesc())
                 .willReturn(Optional.of(Account.builder()
-                        .accountStatus(AccountStatus.UNREGISTERED)
-                        .accountNumber("6789")
-                        .build()));
-        // when
-        Account account = accountService.getAccount(455L);
+                        .accountNumber("1000000012").build()));
 
+        given(accountRepository.save(any()))
+                .willReturn(Account.builder()
+                        .accountUser(user)
+                        .accountNumber("1000000015").build());
+        ArgumentCaptor<Account> captor = ArgumentCaptor.forClass(Account.class);
+
+        // when
+        AccountDto accountDto = accountService.createAccount(1L, 1000L);
         // then
-        assertEquals("6789", account.getAccountNumber());
-        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
+        verify(accountRepository, times(1)).save(captor.capture());
+        assertEquals(12L, accountDto.getUserId());
+        assertEquals("1000000013", captor.getValue().getAccountNumber());
     }
 
-    @Test
-    @DisplayName("계좌 조회 실패 - 음수 조회")
-    void testGetAccountFail() {
-        // given
-        // when
-        RuntimeException exception = assertThrows(RuntimeException.class,
-                () -> accountService.getAccount(-30L));
-        // then
-        assertEquals("Minus", exception.getMessage());
-    }
 
-    @Test
-    @DisplayName("테스트 이름 변경")
-    void testGetAccount() {
-        // given
-        given(accountRepository.findById(anyLong()))
-                .willReturn(Optional.of(Account.builder()
-                        .accountStatus(AccountStatus.UNREGISTERED)
-                        .accountNumber("6789")
-                        .build()));
-        ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
-        // when
-        Account account = accountService.getAccount(455L);
-
-        // then
-        verify(accountRepository, times(1)).findById(captor.capture());
-        verify(accountRepository, times(0)).save(any());
-        assertEquals(455L, captor.getValue());
-        assertNotEquals(45556L, captor.getValue());
-        assertTrue(455L == captor.getValue());
-        assertEquals("6789", account.getAccountNumber());
-        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
-    }
-
-    @Test
-    void testGetAccount2() {
-        // given
-        given(accountRepository.findById(anyLong()))
-                .willReturn(Optional.of(Account.builder()
-                        .accountStatus(AccountStatus.UNREGISTERED)
-                        .accountNumber("6789")
-                        .build()));
-        // when
-        Account account = accountService.getAccount(455L);
-
-        // then
-        assertEquals("6789", account.getAccountNumber());
-        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
-    }
 
 }
